d979612bbda1201e7ae9eccc5439d1e35b1352fa
Zeleo21 <leo.guazzaroni@epita.fr>
2026-05-21 20:01:29 +0000

    feat: rebranded the widget and some cool animations now

--- portfolio/src/app/profile/profile.ts
+++ portfolio/src/app/profile/profile.ts
@@ -1,8 +1,9 @@
 import {afterNextRender, Component, inject} from '@angular/core';
 import {Tag, Theme} from '../components/tag/tag';
-import { splitText, createTimeline } from 'animejs';
+import {splitText, createTimeline, stagger} from 'animejs';
 import {AnimateService} from '../service/animate/animate.service';
 import {Router} from '@angular/router';
+
 
 @Component({
   selector: 'app-profile',
@@ -29,16 +30,44 @@ export class Profile {
           ease: 'outQuart',
           duration: 600,
         },
-        loop: true,
         loopDelay: 5000,
       })
 
       tl.add(engineerFullstackChars, this.animateService.getAppearingTextConfigWithSpecificStagger(this.engineerFullStackCharsStaggerDelay));
       tl.add(descriptionWords, this.animateService.getAppearingTextConfigWithSpecificStagger(this.descriptionWordsStaggerDelay));
+      tl.add('.reveal-grid', {
+        duration: 500,
+        opacity: {
+          from: 0,
+          to: 1,
+        },
+        translateY: {
+          from: -200,
+          to: 0
+        },
+        ease: 'inOutBack(0)',
+      })
+      const commonConfig = {
+        duration: 500,
+        opacity: {
+          from: 0,
+          to: 1
+        },
+        delay: stagger(100),
+        ease: 'inOutSine'
+      }
+      const categories = ['.reveal-frontend', '.reveal-backend', '.reveal-infra', '.reveal-learning'];
+      for(let i = 0 ; i < categories.length ; i++){
+        if (i % 2 === 0) {
+          tl.add(categories[i], { ...commonConfig, translateX: { from: -50, to: 0 } });
+        } else {
+          tl.add(categories[i], { ...commonConfig, translateX: { from: 50, to: 0 } });
+        }
+      }
     });
   }
 
-  navigateToProjects() {
+  protected navigateToProjects() {
     this.router.navigate(['/projects']);
   }
 }
